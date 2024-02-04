712bc1024572b6b6344fed6e1d166c120cc5c980
sals1995 <58127260+sals1995@users.noreply.github.com>
2024-02-04 02:23:50 +0200

    getting started

--- src/app/heroes/heroes.component.intgration.spec.ts
+++ src/app/heroes/heroes.component.intgration.spec.ts
@@ -8,58 +8,60 @@ import { By } from "@angular/platform-browser"
 import { HeroComponent } from "../hero/hero.component"
 
 @Component({
-    selector:"app-hero",
-  standalone: true,
-    template:"<div></div>"
+    selector: "app-hero",
+    standalone: true,
+    template: "<div></div>"
 })
-class HeroComponentMock{
+class HeroComponentMock {
     @Input() hero!: Hero;
 }
 
 
 fdescribe("heroes component (integration)", () => {
-    let component: HeroesComponent,mockHeroes:Hero[],mockHeroService:jasmine.SpyObj<HeroService>,fixture:ComponentFixture<HeroesComponent>
-    beforeEach(()=>{
-        mockHeroes=[
-            {id:100,name: "super man",strength:17},
-            {id:103,name: "bat man",strength:6},
-            {id:102,name: "bat man2",strength:6},
+    let component: HeroesComponent, mockHeroes: Hero[], mockHeroService: jasmine.SpyObj<HeroService>, fixture: ComponentFixture<HeroesComponent>
+    beforeEach(() => {
+        mockHeroes = [
+            { id: 100, name: "super man", strength: 17 },
+            { id: 103, name: "bat man", strength: 6 },
+            { id: 102, name: "bat man2", strength: 6 },
         ]
-         mockHeroService= jasmine.createSpyObj(["getHeroes","deleteHero"])
-        mockHeroService.getHeroes.and.returnValue( of(mockHeroes) )
+        mockHeroService = jasmine.createSpyObj(["getHeroes", "deleteHero"])
+        mockHeroService.getHeroes.and.returnValue(of(mockHeroes))
+
+        TestBed.configureTestingModule({
+            imports: [HeroesComponent]
+        })
+        .overrideComponent(HeroesComponent, {
+            set: {
+            //    imports: [HeroComponentMock],
+                providers: [
+                    { provide: HeroService, useValue: mockHeroService }
+                ]
+            },
+        })
+        fixture = TestBed.createComponent(HeroesComponent)
+        component = fixture.componentInstance
         
-         TestBed.configureTestingModule({
-            imports:[HeroesComponent],
-            providers:[
-                {provide:HeroService,useValue:mockHeroService}
-            ]
-          })//.overrideComponent(HeroesComponent,{
-        //     remove:{imports:[HeroComponent]},
-        //     add:{imports:[HeroComponentMock]},
-        //  })//
-       fixture= TestBed.createComponent(HeroesComponent)
-       component= fixture.componentInstance
-    //    fixture.detectChanges()
     })
-    it("expect heroes[] to be empty",()=>{
+    it("expect heroes[] to be empty", () => {
         expect(component.heroes.length).toBe(0)
     })
-    it("expect heroes to be set successfully when ngOninit() and template detect changes",()=>{
-        
+    it("expect heroes to be set successfully when ngOninit() and template detect changes", () => {
+
         component.ngOnInit()
         expect(component.heroes.length).withContext("length").toBe(3)
         expect(component.heroes[0].name).withContext("name of hero in heroes[]").toBe(mockHeroes[0].name)
         expect(mockHeroService.getHeroes).toHaveBeenCalled()
 
         fixture.detectChanges()
-        let liTags= fixture.debugElement.queryAll(By.css("app-hero"))
+        let liTags = fixture.debugElement.queryAll(By.css("li"))
         expect(liTags.length).toBe(3)
 
-       let childrenComponents= fixture.debugElement.queryAll(By.directive(HeroComponent))
-       expect(childrenComponents.length).toBe(3)
-       expect(childrenComponents[0].componentInstance.hero.name).toBe(mockHeroes[0].name)
+        let childrenComponents = fixture.debugElement.queryAll(By.directive(HeroComponent))
+        expect(childrenComponents.length).toBe(3)
+        expect(childrenComponents[0].componentInstance.hero.name).toBe(mockHeroes[0].name)
     })
-    it("expect to delete 'super man' successfully when delete()",()=>{
+    it("expect to delete 'super man' successfully when delete()", () => {
         component.ngOnInit()
 
         component.delete(mockHeroes[0])
@@ -67,5 +69,5 @@ fdescribe("heroes component (integration)", () => {
         expect(component.heroes.length).toBe(2)
         expect(mockHeroService.deleteHero).toHaveBeenCalledOnceWith(mockHeroes[0])
     })
-    
+
 })
