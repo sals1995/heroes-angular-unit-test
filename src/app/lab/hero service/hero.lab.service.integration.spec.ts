8373e2e4f3ee8daf320e0912ec7040bed99db3b5
Salsabeel <58127260+sals1995@users.noreply.github.com>
2024-08-18 08:01:20 +0300

    preparation

--- src/app/lab/hero service/hero.lab.service.integration.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("3-hero service (http) integration testing:", () => {
-    
-    it("getHeroes function: send request and receive response successfully", () => { })
-    it("updateHero function: send request and receive response successfully", () => { })
-})
